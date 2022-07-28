379b1537cf4925129e305f70c149801f4736d8cb
Outhman <outhmanfofana@gmail.com>
2022-07-28 22:07:47 +0200

    correction

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Container, Typography, Button, Grid } from '@material-ui/core';
+
+import useStyles from './styles';
+import CartItem from './CartItem/CartItem';
+
+const Cart = ({ cart }) => {
+    const classes = useStyles()
+
+    const EmptyCart = () => (
+        <Typography variant='subtitle1'>Votre Panier Semble Vide, Ajoutez Des Articles Pour le Remplir !</Typography>
+    );
+
+    const FilledCart = () => (
+        <>
+            <Grid container spacing={3}>
+                {cart.line_items.map((item) =>(
+                    <Grid item xs={12} sm={4} key={item.id}> 
+                        <CartItem item={item}/>
+                    </Grid>
+                ))}
+            </Grid>
+            <div className={classes.cardDetails}>
+                <Typography variant='h4'>Total: {cart.subtotal.formatted_with_symbol}</Typography>
+                <div>
+                    <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'>Vider le Panier</Button>
+                    <Button className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Payer</Button>
+                </div>
+            </div>
+        </>
+    );
+
+    if(!cart.line_items) return 'Chargement...';
+
+  return (
+    <Container>
+        <div className={classes.toolbar} />
+        <Typography className={classes.tittle} variant='h3'>Votre Panier</Typography>
+        { !cart.line_items.length ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  )
+}
+
+export default Cart
